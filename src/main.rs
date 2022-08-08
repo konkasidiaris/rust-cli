df7b24c87c6a5cced01a25491a5c7c594008c857
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2022-08-08 23:26:25 +0300

    chore: used anyhow lib for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -12,14 +13,10 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }
